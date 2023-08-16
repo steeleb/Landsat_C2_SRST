90d06229f7b86bba7e61e639a4c7cb28db6eef88
B Steele <B.Steele@colostate.edu>
2023-08-16 15:06:31 -0600

    break down states into grids to not max out mem

--- data_acquisition/src/NHD_Chebyshev_Center_Pull.py
+++ data_acquisition/src/NHD_Chebyshev_Center_Pull.py
@@ -1,6 +1,12 @@
+#in order to successfully run this in RStudio, you will need to initiate the conda env 
+#to do so, type the following in the console:
+#reticulate::use_condaenv(file.path(getwd(), "env"))
+# you will need to have the Landsat_C2_SRST project open.
+
 import ee
 import math
 import time
+
 ee.Initialize(project = 'ee-ls-c2-srst')
 
 ## Deepest point calculation adapted from Xiao Yang
@@ -47,7 +53,7 @@ def GetLakeCenters(polygon):
         geometry=geo,
         scale=scale,
         bestEffort=True,
-        tileScale=1
+        tileScale=2 
     ).getNumber('distance').int16())
 
     outputDp = (ee.Feature(dist.addBands(ee.Image.pixelLonLat()).updateMask(dist.gte(maxDistance))
@@ -61,10 +67,11 @@ def GetLakeCenters(polygon):
         collection=regions,
         properties=['type'],
         scale=scale,
-        tileScale=1,
+        tileScale=2,
         geometries=True)
 
     return (ee.Feature(output.first()).copyProperties(polygon,['permanent','areasqkm']))
+
 
 def buff_dp(dp):
     return dp.buffer(dp.getNumber('distance'))
@@ -91,36 +98,59 @@ def maximum_no_of_tasks(MaxNActive, waitingPeriod):
                 NActive += 1
     return ()
 
+
 def calc_area(feature):
     return(feature.set('area_calc',feature.area().divide(1e6)))
 
+## Get NHD Asset Lists
 assets_done = ee.data.listAssets({'parent': 'projects/ee-ls-c2-srst/assets/NHD_centers'})['assets']
 ids_done = [i['id'].split('/')[-1] for i in assets_done]
 state_assets = ee.data.listAssets({'parent': 'projects/sat-io/open-datasets/NHD'})['assets']
 states_left = [i for i in assets_parent if i['id'].split('/')[-1] not in ids_done]
 #assets_parent = [i for i in assets_parent if i['id'].split('/')[-1] not in ['NHD_MO','NHD_TX','NHD_AK']]
 
+## Get State asset
+states = ee.FeatureCollection('TIGER/2018/States')
 
+## for each state, run each grid cell and then export to file
 for i in range(len(states_left)):
     state_asset = states_left[i]['id']
     state_waterbody = (ee.FeatureCollection(f"{state_asset}/NHDWaterbody")
-    .filter(ee.Filter.gte('areasqkm',0.001))
-    .filter(ee.Filter.lte('areasqkm',5000))  #Remove Great Lakes
-    .filter(ee.Filter.inList('ftype',[361,436,390])))
-
+      .filter(ee.Filter.gte('areasqkm',0.001))
+      .filter(ee.Filter.lte('areasqkm',5000))  #Remove Great Lakes
+      .filter(ee.Filter.inList('ftype',[361,436,390]))) #Only grab lakes, ponds, and reservoirs
+    
     ## Added to trouble shoot NM where some of the NHD areas are don't match the actual polygon areas
     state_waterbody=ee.FeatureCollection(state_waterbody.map(calc_area)).filter(ee.Filter.gte('area_calc',0.001))
+    
+    #get the state outline
+    state_usps = state_asset.split('_')[-1]
+    state_bound = states.filter(ee.Filter.eq('STUSPS', state_usps))
 
-    dp = state_waterbody.map(GetLakeCenters)
+    # break down the state into a grid
+    state_geo = state_bound.geometry()
+    state_grid = ee.Geometry(state_geo).coveringGrid(state_geo.projection())
 
-    dataOut = ee.batch.Export.table.toAsset(collection=dp,
-    description=state_asset.split('/')[-1],
-    assetId=f"projects/ee-ls-c2-srst/assets/NHD_centers/{state_asset.split('/')[-1]}")
+    grid_count = state_grid.size().getInfo()
 
-    ## Check how many existing tasks are running and take a break if it's >15
-    maximum_no_of_tasks(15, 240)
-    ## Send next task.
-    dataOut.start()
+    for g in range(grid_count):
+      grid_cell = state_grid.toList(state_grid.size()).get(g)
+      grid_cell = ee.Feature(grid_cell)
+      grid_waterbody = state_waterbody.filterBounds(grid_cell.geometry())
+      
+      if grid_waterbody.size().getInfo() > 0:
+    
+        csc = grid_waterbody.map(GetLakeCenters)
+  
+        dataOut = (ee.batch.Export.table.toAsset(collection=dp,
+          description=state_asset.split('/')[-1]+'_'+str(g)+'_'+str(grid_count),
+          assetId=f"projects/ee-ls-c2-srst/assets/NHD_centers/{state_asset.split('/')[-1]}_{str(g)}"))
+  
+        ## Check how many existing tasks are running and take a break if it's >15
+        maximum_no_of_tasks(5, 240)
+        ## Send next task.
+        dataOut.start()
+      
     print(state_asset.split('/')[-1])
 
 
