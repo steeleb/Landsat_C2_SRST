e59f2a9e62bdf433498b88aeacd1c983921712d4
B Steele <B.Steele@colostate.edu>
2024-07-24 10:49:10 -0600

    update pysetup

--- data_acquisition/py/pySetup.R
+++ data_acquisition/py/pySetup.R
@@ -1,18 +1,23 @@
-# this script sets up a python virtual environment for use in this workflow
 
-library('reticulate')
+library(reticulate)
 
-try(install_miniconda())
+# NOTE, IF YOU ALTER THIS SCRIPT, YOU WILL NEED TO DELETE THE 'ENV' FOLDER
+# SO THAT CHANGES ARE REBUILT IN NEW VENV
 
-py_install(envname = 'env/', 
-           c('earthengine-api', 'pandas', 'fiona', 'pyreadr'), 
-           python_version = 3.8)
-
-#create a conda environment named 'env' with the packages you need
-conda_create(envname = file.path(getwd(), 'env'),
-             python_version = 3.8,
-             packages = c('earthengine-api', 'pandas', 'fiona', 'pyreadr'))
-
-Sys.setenv(RETICULATE_PYTHON = file.path(getwd(), 'env/bin/python/'))
-
-use_condaenv(file.path(getwd(), "env/"))
+# activate conda env or create conda env and modules for the venv
+tryCatch(use_condaenv(file.path(getwd(), 'env')),
+         warning = function(w){
+           print("conda environment activated")
+         },
+         error = function(e) {
+           # install miniconda if necessary
+           try(install_miniconda())
+           #create a conda environment named 'mod_env' with the packages you need
+           conda_create(envname = file.path(getwd(), 'env'))
+           conda_install(envname = 'env/', 
+                         packages = c('earthengine-api', 'pandas', 'fiona', 'pyreadr'))
+           # set the new python environment
+           use_condaenv(file.path(getwd(), "env/"))
+           print("conda environment created and activated")
+         }
+)
